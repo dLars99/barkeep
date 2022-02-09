f1f01bde186540eecd9402a7b481cfadfa4e7623
David Larsen <davidlarsen@gmail.com>
2022-02-08 22:20:39 -0600

    fix(cors): Add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import typesRouter from "./routes/types.routes";
 import categoriesRouter from "./routes/categories.routes";
 import ingredientsRouter from "./routes/ingredients.routes";
@@ -8,16 +9,17 @@ const app = express();
 const port = 8080; // default port to listen
 const version = process.env.npm_package_version;
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', typesRouter);
-app.use('/', categoriesRouter);
-app.use('/', ingredientsRouter);
-app.use('/', recipesRouter)
-
+app.use("/", typesRouter);
+app.use("/", categoriesRouter);
+app.use("/", ingredientsRouter);
+app.use("/", recipesRouter);
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server v${version} started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  console.log(`server v${version} started at http://localhost:${port}`);
+});
