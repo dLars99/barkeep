abf1d9235997f71ca845103de2a98070cde03a61
David Larsen <54251395+dLars99@users.noreply.github.com>
2022-07-11 21:53:44 -0500

    fix(docker): Dockerfile ENV fixes (#15)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:14.18
 WORKDIR /usr/src/app
 ENV NODE_ENV=development
+ENV DOCKER=true
 COPY package.json ./
 COPY yarn.lock ./
 COPY tsconfig.json ./
@@ -11,10 +12,11 @@ RUN yarn build
 FROM node:14.18
 WORKDIR /usr/src/app
 ENV NODE_ENV=production
+ENV DOCKER=true
 COPY package.json ./
 COPY yarn.lock ./
 COPY tsconfig.json ./
-RUN rm -rf node_modules && yarn install --frozen-lockfile
+RUN rm -rf node_modules && yarn install --frozen-lockfile --production
 COPY --from=0 /usr/src/app/dist .
 EXPOSE 8080
 CMD ["node", "index.js"]
--- src/util/db.ts
+++ src/util/db.ts
@@ -1,25 +1,30 @@
-import * as pg from 'pg';
-import knex, {Knex} from "knex";
+import * as pg from "pg";
+import knex, { Knex } from "knex";
 
 const NODE_ENV = process.env.NODE_ENV;
+const DOCKER = process.env.DOCKER;
 const DATABASE_URL = process.env.DATABASE_URL as string;
 
-if (NODE_ENV !== 'development') {
+const isDocker = DOCKER === "true" ? true : false;
+
+/* If running on a local docker instance, this is not needed */
+if (NODE_ENV !== "development" && !isDocker) {
   pg.defaults.ssl = {
     rejectUnauthorized: false,
   };
 }
 
 const config = {
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: DATABASE_URL,
-    ssl: NODE_ENV !== 'development' && {
-      rejectUnauthorized: false,
-    },
+    ssl: NODE_ENV !== "development" &&
+      !isDocker && {
+        rejectUnauthorized: false,
+      },
   },
-  debug: NODE_ENV === 'development',
-}
+  debug: NODE_ENV === "development",
+};
 
 const db: Knex = knex(config as Knex.Config);
 export default db;
